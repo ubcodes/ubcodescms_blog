995442b6d9ada62cd51e5948efc4588ce3bed4bb
UB30-4 <101749091+UB30-4@users.noreply.github.com>
2023-04-19 09:44:10 +0100

    ogtest4

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Logo from '../public/logo1.png'
 
 
 
-export default function Home({ posts }) {
+export default function Home({ posts, author }) {
   return (
     <div>
  <div className="container mx-auto px-10 mb-8 ">
@@ -15,7 +15,7 @@ export default function Home({ posts }) {
         <title>Ubcodes | Blog</title>
         <meta name="description" content="Articles on web development, books and mental health" />
         <meta property="og:title" content="My personal blog" />
-        <meta property="og:image" content="https://og-examples.vercel.sh/api/static" />
+        <meta property="og:image" content={author.photo.url} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <FeaturedPosts />
