1e8a4ab5cb0ffe1263770c03e9ebf899a6bb228b
UB30-4 <101749091+UB30-4@users.noreply.github.com>
2023-08-03 12:07:58 +0100

    og image update

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,14 @@ export default function Home({ posts}) {
       <Head> 
         <title>Ubcodes | Blog</title>
      
-        <meta property="og:image" content="https://pbs.twimg.com/media/Fx4HQ-_X0AMfOL3?format=jpg&name=900x900" />
+        <meta property="og:image" content="https://pbs.twimg.com/media/F1V6Az9WAAkZRLf?format=jpg&name=large" />
 
       <meta name="keywords" content="HTML,CSS, JavaScript, Mental Health, React, Books" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@ubcodes1_" />
       <meta name="twitter:title" content="Ubcodes | Blog" />
       <meta name="twitter:description" content="Welcome to my blog, where I share my insights and tips on web development, mental health and books. Whether you are a beginner or an expert, a reader or a writer, a coder or a therapist, you will find something useful and interesting here." />
-      <meta name="twitter:image" content="https://pbs.twimg.com/media/Fx4HQ-_X0AMfOL3?format=jpg&name=900x900" />
+      <meta name="twitter:image" content="https://pbs.twimg.com/media/F1V6Az9WAAkZRLf?format=jpg&name=large" />
 
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Ubcodes | Blog" />
